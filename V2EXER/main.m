b2f69d386f8672b8dd002e88c76ecccd8c4dcd5f
xujialiang <mail@xujialiang.net>
2014-12-09 09:36:41 +0800

    init

---
+++ V2EXER/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  V2EXER
+//
+//  Created by jialiang.xu on 14/12/9.
+//  Copyright (c) 2014年 xujialiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
